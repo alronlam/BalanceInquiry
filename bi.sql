c41fc8e91a080d846f6877eda2154c5dc5dc1d42
Alron <alron@apollo.com.ph>
2015-07-08 14:11:08 +0800

    Added drop database and insert statements in SQL script.

--- bi.sql
+++ bi.sql
@@ -1,3 +1,4 @@
+DROP DATABASE balance_inquiry;
 CREATE DATABASE balance_inquiry;
 
 USE balance_inquiry;
@@ -10,3 +11,9 @@ CREATE TABLE logs (id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 		   account_number VARCHAR(20) NOT NULL,
 		   transaction_time TIMESTAMP DEFAULT NOW(),
 		   response VARCHAR(20) NOT NULL);
+
+INSERT INTO accounts (account_number, balance) VALUES (1000, 1000);
+INSERT INTO accounts (account_number, balance) VALUES (2000, 10000);
+INSERT INTO accounts (account_number, balance) VALUES (3000, 100000);
+INSERT INTO accounts (account_number, balance) VALUES (4000, 1000000);
+INSERT INTO accounts (account_number, balance) VALUES (5000, 10000000);
