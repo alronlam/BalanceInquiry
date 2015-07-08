1dc2e90299054725d075f18f1f18f068c9eb4987
Alron <alron@apollo.com.ph>
2015-07-08 13:56:45 +0800

    Created stub classes for the different participants.

---
+++ src/main/java/BalanceInquiry/transaction/participant/ValidateFields.java
@@ -0,0 +1,26 @@
+package transaction.participant;
+
+import java.io.Serializable;
+
+import org.jpos.transaction.TransactionParticipant;
+
+public class ValidateFields implements TransactionParticipant {
+
+	@Override
+	public void abort(long id, Serializable context) {
+		System.out.println("\n*****\n Abort\n*****\n");
+	}
+
+	@Override
+	public void commit(long id, Serializable context) {
+		System.out.println("\n*****\n Commit\n*****\n");
+	}
+
+	@Override
+	public int prepare(long id, Serializable context) {
+		System.out.println("\n*****\n Prepare\n*****\n");
+
+		return PREPARED;
+	}
+
+}
