b70b8f544485fd46482998b356f9c13762e10b48
Alron <alron@apollo.com.ph>
2015-07-08 15:37:31 +0800

    Added a few constants in Constant.java that is being used by the CreateInquiryResponse participant. Also added a few print statements to the latter class to be executed when in debug mode.

--- src/main/java/BalanceInquiry/constant/Constant.java
+++ src/main/java/BalanceInquiry/constant/Constant.java
@@ -1,10 +1,16 @@
 package constant;
 
-public class Constant{
+public class Constant {
 
-  public static final int REQUEST = 0;
-  public static final int ISOSOURCE = 1;
+	public static final int REQUEST = 0;
+	public static final int ISOSOURCE = 1;
+	public static final int RESPONSE = 2;
 
-  public static final String VALIDATE_FIELDS_REQUIRED = "required";
+	public static final String BALANCE = "balance";
+
+	public static final String VALIDATE_FIELDS_REQUIRED = "required";
+
+	public static final String RESPONSE_OK = "ok";
+	public static final String RESPONSE_NO = "no";
 
 }
--- src/main/java/BalanceInquiry/transaction/participant/CreateInquiryResponse.java
+++ src/main/java/BalanceInquiry/transaction/participant/CreateInquiryResponse.java
@@ -2,25 +2,70 @@ package transaction.participant;
 
 import java.io.Serializable;
 
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.transaction.Context;
 import org.jpos.transaction.TransactionParticipant;
 
-public class CreateInquiryResponse implements TransactionParticipant {
+import constant.Constant;
+
+public class CreateInquiryResponse implements TransactionParticipant, Configurable {
+
+	private Configuration cfg;
 
 	@Override
 	public void abort(long id, Serializable context) {
-		System.out.println("\n*****\n Abort\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nCreateInquiryResponse Abort\n*****\n");
 	}
 
 	@Override
 	public void commit(long id, Serializable context) {
-		System.out.println("\n*****\n Commit\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nCreateInquiryResponse Commit\n*****\n");
+
+		ISOMsg responseMsg = (ISOMsg) ((Context) context).get(Constant.REQUEST);
+		try {
+
+			Object balance = ((Context) context).get(Constant.BALANCE);
+
+			if (balance != null) {
+
+				responseMsg.set(39, Constant.RESPONSE_OK);
+				responseMsg.set(4, balance.toString());
+
+				if (cfg.getBoolean("debug", true))
+					System.out.println("\n*****\nCreateInquiryResponse Commit: Balance is "+balance.toString()+"\n*****\n");
+
+			} else{
+
+				responseMsg.set(39, Constant.RESPONSE_NO);
+
+				if (cfg.getBoolean("debug", true))
+					System.out.println("\n*****\nCreateInquiryResponse Commit: Balance is unavailable\n*****\n");
+
+			}
+
+			((Context) context).put(Constant.RESPONSE, responseMsg);
+		} catch (ISOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public int prepare(long id, Serializable context) {
-		System.out.println("\n*****\n Prepare\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nCreateInquiryResponse Prepare\n*****\n");
 
 		return PREPARED;
+	}
+
+	@Override
+	public void setConfiguration(Configuration cfg) throws ConfigurationException {
+		this.cfg = cfg;
 	}
 
 }
