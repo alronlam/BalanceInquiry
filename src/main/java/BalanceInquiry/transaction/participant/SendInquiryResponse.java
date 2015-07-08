1851122779131d6dd0f5b9d6366ee5a8a6674627
Alron <alron@apollo.com.ph>
2015-07-08 15:43:08 +0800

    Filled in the SendInquiryResponse class.

--- src/main/java/BalanceInquiry/transaction/participant/SendInquiryResponse.java
+++ src/main/java/BalanceInquiry/transaction/participant/SendInquiryResponse.java
@@ -1,26 +1,56 @@
 package transaction.participant;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import org.jpos.core.Configurable;
+import org.jpos.core.Configuration;
+import org.jpos.core.ConfigurationException;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.ISOMsg;
+import org.jpos.iso.ISOSource;
+import org.jpos.transaction.Context;
 import org.jpos.transaction.TransactionParticipant;
 
-public class SendInquiryResponse implements TransactionParticipant {
+import constant.Constant;
+
+public class SendInquiryResponse implements TransactionParticipant, Configurable {
+
+	private Configuration cfg;
 
 	@Override
 	public void abort(long id, Serializable context) {
-		System.out.println("\n*****\n Abort\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nSendInquiryResponse Abort\n*****\n");
 	}
 
 	@Override
 	public void commit(long id, Serializable context) {
-		System.out.println("\n*****\n Commit\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nSendInquiryResponse Commit\n*****\n");
+
+		ISOSource sender = (ISOSource) ((Context) context).get(Constant.ISOSOURCE);
+		ISOMsg responseMsg = (ISOMsg) ((Context) context).get(Constant.RESPONSE);
+		try {
+			sender.send(responseMsg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ISOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public int prepare(long id, Serializable context) {
-		System.out.println("\n*****\n Prepare\n*****\n");
+		if (cfg.getBoolean("debug", true))
+			System.out.println("\n*****\nSendInquiryResponse Prepare\n*****\n");
 
 		return PREPARED;
+	}
+
+	@Override
+	public void setConfiguration(Configuration arg0) throws ConfigurationException {
+		this.cfg = cfg;
 	}
 
 }
